5da1c6b23c304719f5ffd5f7dc603738d1a791c8
reynoldadade <reynoldadade@gmail.com>
2022-08-30 19:11:21 +0000

    node learning

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const http = require('http');
-const routes = require('./routes');
 
-// //create request listener
-// function requestListener(req, res) {
-//   //   res.writeHead(200, { "Content-Type": "text/plain" });
-//   //   res.end("Hello World\n");
-// }
-//create server
-const server = http.createServer(routes);
+// add express
+const express = require('express');
 
-server.listen(3000);
+const app = express();
+
+// handle middlewares
+app.use('/add-product', (request, response, next) => {
+	console.log('in another middleware');
+	// send allows us to send a response back
+	response.send('<h1>The add product page</h1>');
+});
+
+app.use('/', (request, response, next) => {
+	console.log('in the middleware');
+	// next allows us to move to the next middleware in line
+	response.send('<h1>The default index</h1>');
+});
+
+// const server = http.createServer(app);
+
+// server.listen(3000);
+app.listen(3000);
